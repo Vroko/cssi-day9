ee277f65a928e83ecfed77ec2e823857cd0561c2
Jonathan Turnage <jetx2turnage@gmail.com>
2021-07-22 19:43:08 +0000

    Added functionalty for writing a message to the database

--- public/js/writeMessage.js
+++ public/js/writeMessage.js
@@ -2,12 +2,33 @@ const submitMessage = () => {
     const passcode = document.querySelector("#passcode").value;
     const message = document.querySelector("#message").value;
 
-   firebase.database().ref("/messages").push({
+    if(checkPasscode(passcode))
+    {
+        firebase.database().ref("/messages").push({
         passcode: passcode,
         message: message
-    });
-    
-    /*var MD5 = new Hashes.MD5().hex(passcode)
-    // output into DOM
-    console.log(MD5);*/
+        });
+    }
+    else
+    {
+        alert("Password does not contain number or uppercase letter!");
+    }
+}
+
+function checkPasscode(passcode){
+    var checks = new RegExp("^(?=.*[A-Z])(?=.*\\d)");
+    if(checks.test(passcode))
+    {
+        return true;
+    }
+    else return false;
+}
+
+const messageText = document.querySelector("#message");
+
+function checkLength() {
+    if(messageText.value.length >= 119)
+    {
+        alert("Message is too long!");
+    }
 }
